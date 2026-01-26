9c814e74da162eb5e2ae3bd124eab87f7bb50cf6
Fariz Ardin Adhiyaksa <fariz.ardin@gmail.com>
2026-01-27 00:37:11 +0700

    fix error cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const cors = require('cors');
 const ROUTES = require('./routes');
 
 class App {
@@ -10,6 +11,20 @@ class App {
   }
 
   middlewares() {
+    // Konfigurasi CORS untuk mengizinkan akses dari frontend
+    const corsOptions = {
+      origin: [
+        'https://rizurin.my.id',
+        'http://rizurin.my.id',
+        'http://localhost:3000', // untuk development
+        'http://localhost:5173', // untuk vite development
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+      credentials: true, // Jika menggunakan cookies/session
+    };
+    
+    this.app.use(cors(corsOptions));
     this.app.use(express.json());
   }
 
